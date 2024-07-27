f85244d3304a94eb201523fe4cb376aadd20a687
CarrieCanDo <carrie.a.wright@outlook.com>
2024-07-26 21:44:55 -0400

    Committing Assignment

--- src/main/java/school/management/system/Student.java
+++ src/main/java/school/management/system/Student.java
@@ -12,46 +12,97 @@ public class Student {
     private int feesTotal;
 
     /**
-     * Role of the constructor is to create a new student object by initializing the values
-     * Fees for every student is $30,000/year
+     * To create a new student by initializing.
+     * Fees for every student is $30,000.
      * Fees paid initially is 0.
-     *
-     * @param id    id for the student: unique
-     * @param name  name of the student
-     * @param grade grade of the student
+     * @param id id for the student: unique.
+     * @param name name of the student.
+     * @param grade grade of the student.
      */
-
-    public Student(int id, String name, int grade) {
-        this.feesPaid = 0;
-        this.feesTotal = 30000;
-        this.id = id;
-        this.name = name;
-        this.grade = grade;
-
-        //Not going to alter student's name, student's id.
-
-        /**
-         * Used to update the student's grade.
-         * @param grade new grade of the student
-         */
-
-        public void setGrade(int grade) {
-            this.grade = grade;
-
-            /**
-             * Keep adding the fees to feesPaid Field.
-             * Add the fees to the fees paid
-             * The school is going to receive the funds
-             * @param fees fees that the student pays
-             */
-
-            public void updateFeesPaid(int fees){
-                feesPaid += fees;
-
-            }
-        }
-
+    public Student(int id, String name,int grade){
+        this.feesPaid=0;
+        this.feesTotal=30000;
+        this.id=id;
+        this.name=name;
+        this.grade=grade;
 
     }
 
+    //Not going to alter student's name, student's id.
+
+
+    /**
+     * Used to update the student's grade.
+     * @param grade new grade of the student.
+     */
+    public void setGrade(int grade){
+        this.grade=grade;
+    }
+
+
+    /**
+     * Keep adding the fees to feesPaid Field.
+     * Add the fees to the fees paid.
+     * The school is going receive the funds.
+     *
+     * @param fees the fees that the student pays.
+     */
+    public void payFees(int fees){
+        feesPaid+=fees;
+        School.updateTotalMoneyEarned(feesPaid);
+    }
+
+    /**
+     *
+     * @return id of the student.
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     *
+     * @return name of the student.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     *
+     * @return the grade of the student.
+     */
+    public int getGrade() {
+        return grade;
+    }
+
+    /**
+     *
+     * @return fees paid by the student.
+     */
+    public int getFeesPaid() {
+        return feesPaid;
+    }
+
+    /**
+     *
+     * @return the total fees of the student.
+     */
+    public int getFeesTotal() {
+        return feesTotal;
+    }
+
+    /**
+     *
+     * @return the remaining fees.
+     */
+    public int getRemainingFees(){
+        return feesTotal-feesPaid;
+    }
+
+    @Override
+    public String toString() {
+        return "Student's name :"+name+
+                " Total fees paid so far $"+ feesPaid;
+    }
 }
